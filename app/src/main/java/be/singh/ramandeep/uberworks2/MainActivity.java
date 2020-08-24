7aa510c349ecb68f53a11a0553d48ec8f3511f34
sing6709 <ramandeep-singh25@outlook.com>
2020-08-24 14:41:12 +0200

    Added search functionality using recycle views, recycle adapters, filters

--- app/src/main/java/be/singh/ramandeep/uberworks2/MainActivity.java
+++ app/src/main/java/be/singh/ramandeep/uberworks2/MainActivity.java
@@ -2,18 +2,28 @@ package be.singh.ramandeep.uberworks2;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActionBarDrawerToggle mToggle;
+    private RecyclerView recyclerView;
+    private RecyclerAdapter recyclerAdapter;
+
+    List<String> usersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +58,54 @@ public class MainActivity extends AppCompatActivity {
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        usersList = new ArrayList<>();
+        usersList.add("Alpha");
+        usersList.add("Beta");
+        usersList.add("Charlie");
+        usersList.add("Delta");
+        usersList.add("Echo");
+        usersList.add("Foxtrot");
+        usersList.add("Golf");
+        usersList.add("Hotel");
+        usersList.add("India");
+        usersList.add("Juliet");
+        usersList.add("Kilo");
+        usersList.add("Lima");
+        usersList.add("Mark");
+        usersList.add("Nano");
+        usersList.add("Opera");
+        usersList.add("Paul");
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerAdapter = new RecyclerAdapter(usersList);
+        recyclerView.setAdapter(recyclerAdapter);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation_menu, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                recyclerAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
---
+++ app/src/main/java/be/singh/ramandeep/uberworks2/RecyclerAdapter.java
@@ -0,0 +1,103 @@
+package be.singh.ramandeep.uberworks2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> implements Filterable {
+    private static final String TAG = "RecyclerAdapter";
+    List<String> usersList;
+    List<String> usersListAll;
+
+    public RecyclerAdapter(List<String> usersList){
+        this.usersList = usersList;
+        this.usersListAll = new ArrayList<>(usersList);
+    }
+
+    @NonNull
+    @Override
+    public RecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.activity_customers_maps, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapter.ViewHolder holder, int position) {
+        holder.rowCountTextView.setText(String.valueOf(position));
+        holder.textView.setText(usersList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return usersList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+
+    Filter filter = new Filter() {
+        //run on background thread
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            List<String> filteredList = new ArrayList<>();
+
+            if (charSequence.toString().isEmpty()) {
+                filteredList.addAll(usersListAll);
+            }
+            else{
+                for(String user: usersListAll){
+                    if(user.toLowerCase().contains(charSequence.toString().toLowerCase())){
+                        filteredList.add(user);
+                    }
+                }
+            }
+
+            FilterResults filterResults = new FilterResults();
+            filterResults.values = filteredList;
+
+            return filterResults;
+        }
+
+        //runs on a UI thread
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            usersList.clear();
+            usersList.addAll((Collection<? extends String>) filterResults.values);
+            notifyDataSetChanged();
+        }
+    };
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView imageView;
+        TextView textView, rowCountTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.imageView);
+            textView = itemView.findViewById(R.id.textView);
+            rowCountTextView = itemView.findViewById(R.id.rowCountTextView);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(view.getContext(), usersList.get(getAdapterPosition()), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
